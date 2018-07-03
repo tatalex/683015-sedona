0d7279f03acb2da42dfebf814f525733daf6d119
tatalex <burmina7@gmail.com>
2018-07-03 15:06:14 +0300

    заменила в форме контейнер для календаря на button, изменила направление появления формы в соответствии с ТЗ

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 var link = document.querySelector(".button.search");
 var form = document.querySelector(".search-form");
+var entery = form.querySelector("[name=date-entry]")
 
 link.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	form.classList.toggle("form-show");
+	entery.focus();
  });
